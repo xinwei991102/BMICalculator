51e67a2bbcb632cde714fb12a495f1630eb01892
xinwei991102 <ppfflop@gmail.com>
2019-11-11 11:23:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener {
+            CalculateBMI()
+        }
+    }
+
+    private fun CalculateBMI() {
+        if (editTextHeight.text.isEmpty()) {
+            editTextHeight.setError(getString(R.string.input_error))
+            return
+        }
+        if (editTextWeight.text.isEmpty()) {
+            editTextWeight.setError(getString(R.string.input_error))
+            return
+        }
+
+        val weight = editTextWeight.text.toString().toFloat()
+        val height = editTextHeight.text.toString().toFloat()
+        val bmi = weight / (height/100).pow(2)
+
+        if ( bmi < 18.5 ){
+            textViewBMI.text = String.format("%s %.2f (%s)", getString(R.string.bmi),bmi, getString(R.string.underweight))
+            imageViewProfilePic.setImageResource(R.drawable.under)
+        } else if ( bmi < 25 ) {
+            textViewBMI.text = String.format("%s %.2f (%s)", getString(R.string.bmi),bmi, getString(R.string.normal))
+            imageViewProfilePic.setImageResource(R.drawable.normal)
+        } else {
+            textViewBMI.text = String.format("%s %.2f (%s)", getString(R.string.bmi),bmi, getString(R.string.overweight))
+            imageViewProfilePic.setImageResource(R.drawable.over)
+        }
+    }
+}
